9861c094171017ea2832efa63a23bcd21886a322
Dane Hale <danehale0612@gmail.com>
2013-06-04 14:01:51 -0500

    refactor

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -97,17 +97,29 @@ module MoviesController
   end
 
   def full_movie_info(movie_info)
-    "\nTitle: " + movie_info['Title'] +
-    "\nYear: " + movie_info['Year'] +
-    "\nRated: " + movie_info['Rated'] +
-    "\nReleased: " + movie_info['Released'] +
-    "\nRuntime: " + movie_info['Runtime'] +
-    "\nGenre: " + movie_info['Genre'] +
-    "\nDirector: " + movie_info['Director'] +
-    "\nWriter: " + movie_info['Writer'] +
-    "\nActors: " + movie_info['Actors'] +
-    "\nRotten Tomatoes Score: " + movie_info['tomatoMeter'] +
-    "\nPlot:\n" + movie_info['Plot']
+    "
+    Title: " + movie_info['Title'] +
+    "
+    Year: " + movie_info['Year'] +
+    "
+    Rated: " + movie_info['Rated'] +
+    "
+    Released: " + movie_info['Released'] +
+    "
+    Runtime: " + movie_info['Runtime'] +
+    "
+    Genre: " + movie_info['Genre'] +
+    "
+    Director: " + movie_info['Director'] +
+    "
+    Writer: " + movie_info['Writer'] +
+    "
+    Actors: " + movie_info['Actors'] +
+    "
+    Rotten Tomatoes Score: " + movie_info['tomatoMeter'] +
+    "
+    Plot:
+    " + movie_info['Plot']
   end
 
   def delete_from_already_watched_table(db_movie_title)
