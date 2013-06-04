6c2f5be6a372073871470f618308e762770f9b33
Dane Hale <danehale0612@gmail.com>
2013-06-04 13:44:34 -0500

    refactor

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -181,19 +181,13 @@ module MoviesController
 
 
   def user_screen
-    print "
-
-    User Home Screen 
-
-    1) Recommend movies
-    2) Watchlist
-    3) Already Watched List
-    4) Exit App
-
-    What would you like to do: "
-
+    print "\nUser Home Screen" +
+    "\n\n1) Recommend movies" +
+    "\n2) Watchlist" +
+    "\n3) Already Watched List" +
+    "\n4) Exit App" +
+    "\n\nWhat would you like to do: "
     direction = gets.chomp
-
     if direction == "1"
       print "\e[H\e[2J"
       recommend_screen
@@ -212,32 +206,25 @@ module MoviesController
     end
   end
 
-
-
   def login_screen
     print "See These Movies!\n\n" +
     "Login\n\n" +
     "Type Username: "
     username = gets.chomp 
-
     table_username = User.where(:name => username).first
-
     unless table_username.nil?
       print "\e[H\e[2J"
       puts "Welcome, #{username}!"
       @userID = table_username.id
       user_screen
     else
-      print "This username does not exist.  Would you like to create it?
-      y) yes
-      n) no
-
-      :"
-
+      print "This username does not exist.  Would you like to create it?\n" +
+      "\ny) yes" +
+      "\nn) no" +
+      "\n:"
       input = gets.chomp
-
       if input == "n"
-        print "\e[H\e[2J"
+        clear_screen
         login_screen
       elsif input == "y"
         create_user
@@ -267,7 +254,6 @@ module MoviesController
 
   def movie_info_navigation_options(movie_info, top_five_movies, movie_title, db_movie_title)
     movie_action = gets.chomp
-
     if movie_action == "3"
       clear_screen
       recommended_movies(top_five_movies, movie_title)
@@ -300,23 +286,14 @@ module MoviesController
 
   def movie_info_screen(top_five_movies, movie_title)
     picked_movie_index = navigation_options
-
     picked_movie = top_five_movies[picked_movie_index]
-
     response = Faraday.get "http://www.omdbapi.com/?i=&t=#{picked_movie['Name']}&plot=full&tomatoes=true"
-
     movie_info = JSON.parse(response.body)
-
     clear_screen
-
     movie_info_error(picked_movie, movie_title) if movie_info['Error']
-
     puts full_movie_info(movie_info)
-
     movie_info_navigation(movie_info, top_five_movies, movie_title)
-
   end
-
 
   def recommended_movies(top_five_movies, movie_title)
     puts
@@ -324,9 +301,7 @@ module MoviesController
       puts "#{movie['Index']}) #{movie['Name']}"
     end
     puts 
-
     movie_info_screen(top_five_movies, movie_title)
-
   end
 
   def scrubbed_results(results)
@@ -351,31 +326,20 @@ module MoviesController
 
 
   def recommendation_process(movie_title)
-
     response = Faraday.get "http://www.tastekid.com/ask/ws?q=movie:#{movie_title}//movies&format=JSON&f=see_the3022&k=nzfkmgm3nwvm"
-
     results = JSON.parse(response.body)['Similar']['Results']
-
     puts "Movie title not recognized" if results.length == 0
-    
     scrub_results = scrubbed_results(results)
-
     top_five_movies = get_top_five_movies(scrub_results)
-
     recommended_movies(top_five_movies, movie_title)
-
   end
 
 
 
   def recommend_screen
-
     print "Type in a movie that you liked to get recommendations on: "
     movie_title = gets.chomp
-
     puts
-
     recommendation_process(movie_title)
-    
   end
 end
